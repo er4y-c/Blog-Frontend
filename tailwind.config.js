570f62020d32d9b8f2c909e88b0a20c56fbc396f
Eray Aynacı <73905858+er4y-c@users.noreply.github.com>
2023-07-14 15:10:06 +0300

    Create Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx}"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   mode: "jit",
   theme: {
     extend: {
